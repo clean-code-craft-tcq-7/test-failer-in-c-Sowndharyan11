b5969082c9b2161385896ce8d578034d77bfca08
Sowndharyan11 <83329217+Sowndharyan11@users.noreply.github.com>
2025-08-01 12:32:54 +0530

    Update tshirts.c

--- tshirts.c
+++ tshirts.c
@@ -17,6 +17,7 @@ int testTshirtSize() {
     printf("\nTshirt size test\n");
     assert(size(37) == 'S');
     assert(size(40) == 'M');
+     assert(size(38) == 'M');
     assert(size(43) == 'L');
     printf("All is well (maybe!)\n");
     return 0;
